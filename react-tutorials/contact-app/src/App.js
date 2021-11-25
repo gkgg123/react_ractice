e8c5407c2734fa34e205e6c79b7a656aafec2e7c
gkgg123 <gkggg123@gmail.com>
2021-11-25 19:19:08 +0900

    [ADD] phone number JSON render

--- react-tutorials/contact-app/src/App.js
+++ react-tutorials/contact-app/src/App.js
@@ -3,14 +3,24 @@ import { Component } from 'react';
 import PhoneForm from './components/PhoneForm';
 
 class App extends Component {
-
+  id = 0;
+  state = {
+    information : [],
+  }
   handleCreate = (data) => {
-    console.log(data);
+    const {information} = this.state
+    this.setState({
+      information: information.concat({
+        ...data,
+        id : this.id++,
+      })
+    })
   }
   render() {    
     return (
       <div className="App">
-        <PhoneForm onCreate={ this.handleCreate }/>
+        <PhoneForm onCreate={this.handleCreate} />
+        {JSON.stringify(this.state.information)}
       </div>
     );
   }
