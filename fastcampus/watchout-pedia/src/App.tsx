8b6fd7f4bb98821851052eda0456c45de8d4edb8
gkgg123 <gkggg123@gmail.com>
2022-03-28 16:59:28 +0900

    [추가] react-dom 기본적인 페이지 제공

--- fastcampus/watchout-pedia/src/App.tsx
+++ fastcampus/watchout-pedia/src/App.tsx
@@ -1,7 +1,19 @@
+import React from "react";
+import TvPage from "./pages/TvPage";
+import TvDetail from "./pages/TvDetail";
+import MovieDetail from "./pages/MovieDetail"
+import MainPage from "./pages/MainPage";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={ <MainPage/> }/>
+        <Route path="/tv" element={<TvPage />} />
+        <Route path="/movie/:id" element={<MovieDetail />} />
+        <Route path="/tv/:id" element={ <TvDetail/>}/>
+      </Routes>
+    </Router>
   );
 }
 
